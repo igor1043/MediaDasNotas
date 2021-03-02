867de24be60ce96dff69dff941ad91ca28bd74a0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-02 07:28:46 -0300

    Update notas_media_total_aprovado_reprovado_final.cpp

--- notas_media_total_aprovado_reprovado_final.cpp
+++ notas_media_total_aprovado_reprovado_final.cpp
@@ -1,8 +1,13 @@
 /*
+Programa em linguagem C ++ que aquisições como notas das 3 unidades de usuário e informações o total e a mídia. Tambem informa se esta aprovado, reprovado ou em exame final.
 Programa em linguagem C++ que solicita as notas das 3 unidades de usuario e informa o total e a media,
 alem de dizer se sera reprovado (<12pontos), fara prova final (>=12 e <21) ou esta aprovado (>12pontos)
 */
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 //Adiciona a biblioteca <iostream>
 #include<iostream>
 
